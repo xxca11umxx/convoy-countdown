cc06a66b8698fd9eaf9c4fcfe9dd271dc24a7d36
xxca11umxx <theeabros17@gmail.com>
2026-07-22 13:06:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,1 @@
-const EVENTS = [
-    {
-        "title": "Imperial Cargo VTC — Grand Opening Convoy 👑",
-        "date": "2026-07-29T20:00:00Z",
-        "server": "To be determined",
-        "route": "Klagenfurt am Wörthersee → Munich Quarry",
-        "banner": "",
-        "link": "https://truckersmp.com/events/35582-%F0%9F%9A%9B-imperial-cargo-vtc%E2%80%94-grand-opening-convoy%F0%9F%91%91"
-    }
-];
+
