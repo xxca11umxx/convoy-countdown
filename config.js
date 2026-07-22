531098f72563375ce9d755040f8546896990a76f
xxca11umxx <theeabros17@gmail.com>
2026-07-22 03:07:02 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const EVENT = {
+
+    title: "ETS2MCG 10th Anniversary Convoy",
+
+    date: "2026-08-09T13:00:00",
+
+    server: "Event Server",
+
+    route: "Check TruckersMP Event Page",
+
+    banner: "",
+
+    link: "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
+
+};
